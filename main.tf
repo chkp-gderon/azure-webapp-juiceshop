59ede959351d9cf7533fdd89c58367722469e693
chkp-gderon <29176555+chkp-gderon@users.noreply.github.com>
2023-02-08 11:49:55 +0100

    removed obsolete provider

--- main.tf
+++ main.tf
@@ -2,20 +2,11 @@
 
 terraform {
   required_providers {
-    inext = {
-      source  = "CheckPointSW/infinity-next"
-      version = "1.0.3"
-    }
-
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.56.0"
     }
   }
-}
-
-provider "inext" {
-  region = "eu"
 }
 
 provider "azurerm" {
